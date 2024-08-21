d01ccc68663afcd429fae5671527993c8552ecea
hiuhihi78 <leduchieu2001x@gmail.com>
2024-08-21 23:12:12 +0700

    add config ant design, fortawsome, tailwin css

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+import { Button } from 'antd'
+
 export function SignIn() {
+
+    const [buttonLoading, setButtonLoading] = useState(false)
+
+    const clickButton = () => {
+        setButtonLoading(true)
+    }
 
     return (
         <>
-            SignIn
+            <div className="bg-blue-500 text-white p-4">
+                This is a Tailwind CSS styled component.
+            </div>
+            <FontAwesomeIcon icon={faUser} />
+            <Button loading={buttonLoading} onClick={clickButton}>SignIn</Button>
         </>
     )
 }
