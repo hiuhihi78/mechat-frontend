9e6213e6c7fb02050a67081437366a7b5d025d9f
hiuhihi78 <leduchieu2001x@gmail.com>
2024-10-02 23:39:17 +0700

    Add NotificationItem component

---
+++ src/components/NotificationItem/index.js
@@ -0,0 +1,40 @@
+import clsx from "clsx";
+import styles from "./NotificationItem.module.scss"
+import { Avatar } from "antd";
+import moment from 'moment';
+import 'moment/locale/vi';
+
+function NotificationItem({ notification }) {
+
+    return (
+        <>
+            {JSON.stringify(notification)}
+            <div className={clsx(styles['container'])}>
+                <div className={clsx(styles['image'])}>
+                    <Avatar src={notification.image} size="large" />
+                </div>
+                <div className={clsx(styles['info'])}>
+                    <div className={clsx(styles['content'])}>
+                        <p>{notification.content}</p>
+                    </div>
+                    <div className={clsx(styles['time'])}>
+                        <p
+                            className={notification.isReaded ? clsx(styles["time_unRead"]) : ''}
+                        >
+                            {moment(notification.createDate).fromNow()}
+                        </p>
+                    </div>
+                </div>
+                <div className={clsx(styles['isReaded'])}>
+                    {notification.isReaded ?
+                        <></>
+                        :
+                        <span className={clsx(styles['circle'])}></span>
+                    }
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default NotificationItem;
